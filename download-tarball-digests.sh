02d67f7b21a2991dfc53d3919dadabfc857d2db2
Alexander Mueller <xelarellum@web.de>
2019-01-03 15:49:03 +0100

    allow redirs when downloading digests

--- download-tarball-digests.sh
+++ download-tarball-digests.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 MANTIS_VER=$1
-curl  http://jaist.dl.sourceforge.net/project/mantisbt/mantis-stable/${MANTIS_VER}/mantisbt-${MANTIS_VER}.tar.gz.digests
+curl  --max-redirs 2 -L http://downloads.sourceforge.net/project/mantisbt/mantis-stable/${MANTIS_VER}/mantisbt-${MANTIS_VER}.tar.gz.digests
 
